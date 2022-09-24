c723f9a4ea8944c247c498c61e9490f2d15cfc5b
ag502 <ag502@naver.com>
2022-09-24 13:56:36 +0900

    add: vue setting

---
+++ vue/templating/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../../utils/images/brownie.jpg" />
+    <h1>TEST</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import "./styles/index.css";
+
+export default Vue.extend({
+  name: "App",
+});
+</script>
+
+<style></style>

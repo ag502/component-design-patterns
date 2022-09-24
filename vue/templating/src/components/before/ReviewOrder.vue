410483e070a5e33d671920b42c07f417e8a303a1
ag502 <ag502@naver.com>
2022-09-24 16:36:44 +0900

    add: Product list 복사

--- vue/templating/src/components/before/ReviewOrder.vue
+++ vue/templating/src/components/before/ReviewOrder.vue
@@ -4,10 +4,32 @@
       <h2 class="text-2xl text-gray-900 font-medium">Review Order</h2>
 
       <!-- ITEM -->
+      <!-- SearchModal의 ITEM과 같음 -->
+      <div class="my-4 -mx-8">
+        <div
+          v-for="{ name, image, description, price } in products"
+          :key="name"
+          class="item space-x-6"
+        >
+          <img
+            class="rounded-full w-24 h-24 object-cover border border-gray-500 shadow-md"
+            :src="image"
+          />
+          <div class="flex flex-col justify-center flex-1">
+            <span class="text-lg text-gray-900">
+              {{ name }}
+            </span>
+            <span class="mt-1 leading-tight font-light text-gray-700">
+              {{ description }}
+            </span>
+          </div>
+          <div class="text-lg text-gray-600 font-bold">${{ price }}</div>
+        </div>
+      </div>
 
       <!-- TOTAL -->
       <div class="flex items-end justify-end text-lg">
-        <span class="mr-8text-gray-70">Total (before tax)</span>
+        <span class="mr-8 text-gray-700">Total (before tax)</span>
         <span class="font-bold text-gray-600"> ${{ totalPrice }} </span>
       </div>
     </div>

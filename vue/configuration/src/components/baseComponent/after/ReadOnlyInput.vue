218fc0d2955fcefb4a9f7d944358b3734b177892
ag502 <ag502@naver.com>
2022-10-03 01:01:31 +0900

    add: base Component 패턴 추가

---
+++ vue/configuration/src/components/baseComponent/after/ReadOnlyInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Base Component를 wrapping 함으로써 노출되는 props를 제한하여, -->
+  <!-- 부적절한 사용을 막고 코드의 가독성을 높인다. -->
+  <Input :modelValue="value" disabled />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import Input from "./Input.vue";
+
+export default Vue.extend({
+  name: "ReadOnlyInput",
+
+  components: {
+    Input,
+  },
+
+  inheritAttrs: false,
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>

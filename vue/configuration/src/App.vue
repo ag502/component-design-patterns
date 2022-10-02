935ee641c44ed061845431cb3fbf41d0c3ca9895
ag502 <ag502@naver.com>
2022-10-03 00:11:57 +0900

    add: breaking down configuration init

--- vue/configuration/src/App.vue
+++ vue/configuration/src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <div id="app">
-    <SearchModalAfter />
-    <ReviewOrderAfter />
+    <!-- <SearchModalAfter /> -->
+    <!-- <ReviewOrderAfter /> -->
+    <TableWithPagination :data="tableData.data" :columns="tableData.cols" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
+import data from "./components/breakingDown/data.json";
+
 import "./styles/index.css";
-import {
-  ReviewOrderAfter,
-  SearchModalAfter,
-} from "./components/configuration/after";
+// import {
+//   ReviewOrderAfter,
+//   SearchModalAfter,
+// } from "./components/configuration/after";
+import { TableWithPagination } from "./components/breakingDown/before";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    SearchModalAfter,
-    ReviewOrderAfter,
+    // SearchModalAfter,
+    // ReviewOrderAfter,
+    TableWithPagination,
+  },
+
+  data() {
+    return {
+      tableData: data,
+    };
   },
 });
 </script>
---
+++ vue/configuration/src/components/breakingDown/before/index.ts
@@ -0,0 +1,1 @@
+export { default as TableWithPagination } from "./TableWithPagination.vue";
